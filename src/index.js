13b313192f893350a3cc31da56d5b2cbef64f305
Dev Jana <devjana@Devs-MBP-2.domain>
2020-05-11 12:14:56 -0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
 
 // put your reducer here!
 
-
-const storeInstance = createStore( )
+// use reducer in store
+const storeInstance = createStore(  );
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
